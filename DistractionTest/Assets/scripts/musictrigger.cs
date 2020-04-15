62c26b4065a2c824d8f97abc2752dc6b8c43a94e
Fria <fria.khorshid@gmail.com>
2020-04-15 20:29:00 +0200

    Implemented rooms with audio

---
+++ DistractionTest/Assets/scripts/musictrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musictrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Play("Room 1");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Stop("Room 1");
+    }
+}
