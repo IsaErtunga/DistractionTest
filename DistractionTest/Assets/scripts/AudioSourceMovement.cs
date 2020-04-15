f00427fc64b58c7737cb24fa151cf9f8b4bc6eb1
IsaErtunga <isa.ertunga@gmail.com>
2020-04-15 14:55:08 +0200

    Audio Source movement

---
+++ DistractionTest/Assets/scripts/AudioSourceMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AudioSourceMovement : MonoBehaviour
+{
+
+    //public Rigidbody rb;
+    private float movementSpeed = 1.5f;
+    Vector3 originalPos;
+
+    void Start()
+    {
+
+        //rb.useGravity = false;
+        originalPos = new Vector3(10.0f, 40.0f, 43.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddForce(0, 0, 100 * Time.deltaTime);
+        gameObject.transform.position = gameObject.transform.position + new Vector3(0, 0, movementSpeed * Time.deltaTime);
+
+
+        if (gameObject.transform.position.z >= 57.0f) {
+            gameObject.transform.position = originalPos;
+        }
+            
+    }
+}
