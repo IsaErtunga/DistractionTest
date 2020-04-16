11e376b0136be0081fc47c6b3abc547ec1568377
Friiie <47301346+Friiie@users.noreply.github.com>
2020-04-16 15:42:34 +0200

    finished with music rooms

---
+++ DistractionTest/Assets/scripts/musictriggerroom2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musictriggerroom2 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Play("Room 2");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Stop("Room 2");
+    }
+}
---
+++ DistractionTest/Assets/scripts/musictriggerroom3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musictriggerroom3 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Play("Room 3");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Stop("Room 3");
+    }
+}
