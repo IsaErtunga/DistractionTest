0765b26ec9d4debcf96c4d5e0d5bd2b5471471d5
Ausama <ausash171@edunet.oru.se>
2020-04-16 18:54:11 +0200

    Incomplete Crowd

---
+++ DistractionTest/Assets/Audience/CrowdMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdMovement : MonoBehaviour
+{
+    static Animator anim;
+    private Rigidbody rb;
+    public float speed = 10F;
+    private int WalkDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        //ChooseDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+    }
+
+    /**public void ChooseDirection()
+    {
+        WalkDirection = Random.Range(0, 4);
+    }
+    */
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.transform.rotation = new Quaternion(0, 90, 0, 0);
+        gameObject.transform.position += new Vector3(0, 0, -(Time.deltaTime * speed));
+    }
+
+
+}
