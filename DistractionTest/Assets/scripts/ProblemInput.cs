f9fcab9599e6f6bba8384a9f70df68a4244d0bc3
HannesHalm <halm.hannes@gmail.com>
2020-04-17 15:47:55 +0200

    Updated ProblemInput to change Scene and handle all problems
    set problems for each scene by doing the following correctAnswers[ currentScene, currentProblem ]
    
    Scene must have filename "ProblemSceneX" X being the number of the room. In order to change scene the scenes have to be included to build files found under File -> Build Settings

--- DistractionTest/Assets/scripts/ProblemInput.cs
+++ DistractionTest/Assets/scripts/ProblemInput.cs
@@ -3,42 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
+// Gets input and checks if its correct move to next problem and when done in a Scene move to next Scene
 public class ProblemInput : MonoBehaviour
 {
     // The input from the user
     public string answer;
 
     // Index for current Problem
-    public int currentProblem = 0;
-    public int amountOfProblems = 3;
+    static public int currentProblem = 0;
+    static public int amountOfProblems = 3;
+    static public int currentScene = 0;
+    static public int amountOfScenes = 3;
 
-    // Correct Answers for problems 1 - 3,  Should be overwritten by the current scene in future
-    public int[] correctAnswers = new int[] {
-            42, 7, 69
-        };
+    // Correct Answers for problems syntax -> correctAnswers[ currentScene, currentProblem ]
+    public int[,] correctAnswers = new int[,] {
+             { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }
+    };
     
     // References to objects in InputUI
     public InputField inputField;
     public GameObject input;
     public GameObject displayAnswer;
 
-
     void Update()
     {
         // Sets focus on inputField
         inputField.ActivateInputField();
-
-        
 
         // Get input and check input when "Enter" key is pressed
         if (Input.GetKeyDown(KeyCode.Return))
         {
             GetInput();
             CheckAnswer();
-            Debug.Log("Correct answers is: " + correctAnswers[currentProblem]);
+            inputField.text = "";
         }
     }
+
     // Update is called once per frame
     public void GetInput()
     {
@@ -52,19 +54,37 @@ public class ProblemInput : MonoBehaviour
     public void CheckAnswer()
     {
         // Checks if answer is correct
-        if (int.Parse(answer) == correctAnswers[currentProblem])
+        if (int.Parse(answer) == correctAnswers[currentScene, currentProblem])
         {
             Debug.Log(answer);
             displayAnswer.GetComponent<Text>().text = "You answered : " + answer + " right!";
+
+            // Move to next problem
             if (currentProblem < amountOfProblems-1)
             {
+                // Increment problem index, stay on the same scene
                 currentProblem++;
+                Debug.Log("Correct answers is: " + correctAnswers[currentScene, currentProblem]);
             }
+
+            // Move to next Scene
+            else if (currentScene < amountOfScenes-1)
+            {
+                // Here timer for scene should stop
+                Debug.Log("You did it your time was: ");
+                currentProblem = 0;
+
+                // Move to next scene, Scene must have name "ProblemSceneX" X being the number of the room
+                currentScene++;
+                SceneManager.LoadScene("ProblemScene" + currentScene.ToString());
+                Debug.Log("Current scene: " + currentScene);
+            }
+
+            // Game end
             else
             {
-                // Stop timer
-                // Move to next scene
-                Debug.Log("You did it your time was: ");
+                // Display times, move to game end Scene
+                Debug.Log("You win your total time was ...");
             }
         }
     }
