df11c858ce3c5e4588f97c0af8e3e0cacc042ed3
swebent <alexander_benteby@hotmail.com>
2020-04-19 17:44:49 +0200

    plssss

--- DistractionTest/Assets/scripts/TimerScript.cs
+++ DistractionTest/Assets/scripts/TimerScript.cs
@@ -9,31 +9,49 @@ public class TimerScript : MonoBehaviour
     public Text stoppedTime;
     private float startTime;
     private bool stopTimer = false;
+    private bool startTimer = false;
+    float time_since_timer_started;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        startTime = Time.time;
+    //void Start()
+    //{
+    //    startTime = Time.time;
 
+    //}
+
+    void Start() {
+        //startTimer = true;
+        //startTime = Time.time;
+        time_since_timer_started = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        time_since_timer_started += Time.deltaTime;
+
         if(stopTimer)
         {
             return;
         }
 
-        float time_since_timer_started = Time.time - startTime;
-        string minutes = ((int)time_since_timer_started / 60).ToString();
-        string seconds = (time_since_timer_started % 60).ToString("f2");
-        timerText.text = minutes + ":" + seconds;
+
+        if (gameObject.activeSelf == true) {
+
+            string minutes = ((int)time_since_timer_started / 60).ToString();
+            string seconds = (time_since_timer_started % 60).ToString("f2");
+            timerText.text = minutes + ":" + seconds;
+        }
+
+        if (gameObject.activeSelf == false) {
+            time_since_timer_started = 0;
+        }
     }
 
     public void StopTimer()
     {
-        stopTimer = true;
         stoppedTime = timerText;
+        startTimer = false;
     }
 }
