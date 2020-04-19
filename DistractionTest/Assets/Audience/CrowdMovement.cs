7dd93ba8a59c4300fa4028f82fccc2e47007a93e
Ausama <ausash171@edunet.oru.se>
2020-04-19 19:17:14 +0200

    ImprovedScript

--- DistractionTest/Assets/Audience/CrowdMovement.cs
+++ DistractionTest/Assets/Audience/CrowdMovement.cs
@@ -17,45 +17,44 @@ public class CrowdMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         target = GetComponent<Collider>();
         myVector = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z);
-        if(rb.name == "NPCR1Forward")
+        if(rb.tag == "NPCForward")
         {
             rb.velocity = myVector + Vector3.forward * speed;
 
         }
-        if (rb.name == "NPCR1Back")
+        if (rb.tag == "NPCBack")
         {
             rb.velocity = myVector + Vector3.back * speed;
         }
-        if (rb.name == "NPCR2Left")
+        if (rb.tag == "NPCLeft")
         {
             rb.velocity = myVector + Vector3.left * speed;
         }
-        if (rb.name == "NPCR2Right")
+        if (rb.tag == "NPCRight")
         {
             rb.velocity = myVector + Vector3.right * speed;
         }
-
     }
 
     private void OnTriggerEnter(Collider other)
     {
         speed = speed * -1;
-        if (rb.name == "NPCR1Forward")
+        if (rb.tag == "NPCForward")
         {
             gameObject.transform.Rotate(0, 180, 0, Space.Self);
             rb.velocity = Vector3.forward * speed;
         }
-        if (rb.name == "NPCR1Back")
+        if (rb.tag == "NPCBack")
         {
             gameObject.transform.Rotate(0, rb.rotation.y - 180, 0, Space.Self);
             rb.velocity = Vector3.back * speed;
         }
-        if (rb.name == "NPCR2Left")
+        if (rb.tag == "NPCLeft")
         {
             gameObject.transform.Rotate(0, rb.rotation.y * 270, 0, Space.Self);
             rb.velocity = Vector3.left * speed;
         }
-        if (rb.name == "NPCR2Right")
+        if (rb.tag == "NPCRight")
         {
             gameObject.transform.Rotate(0, rb.rotation.y * 270, 0, Space.Self);
             rb.velocity = Vector3.right * speed;
